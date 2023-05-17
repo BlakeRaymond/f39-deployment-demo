d03187fc79e30ad9a8e9707c057a33d178fb8b2d
Blake Raymond <blake.crypto@technologist.com>
2023-05-17 13:48:45 -0600

    changed cats name to kosmo

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('button')
 
-const clickHandler = () => alert('The cat name is: Rascal')
+const clickHandler = () => alert('The cat name is: Kosmo')
 
 btn.addEventListener('click', clickHandler)
